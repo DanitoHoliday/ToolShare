dba9311da09dd7e140608588cc59fe04a16f453d
Jan Fuhrmann <zipv3r@gmail.com>
2020-02-25 12:16:22 +0100

    Added Seed file for Users and Tools

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
 
-puts "Creating tools.."
+Tool.destroy_all
+User.destroy_all
 
-Tool.create(name: "Hammer", description: "It's fantastic", user_id: 2)
-Tool.create(name: "Waffle iron", description: "Great stuff", user_id: 3)
+tools = ["Hammer", "Drill", "Iron", "Screwdriver", "Waffle maker", "Lawn cutter", "Bicycle Equipment"]
+
+puts "Creating admin"
+
+x = User.create(username: "admin", email: "admin@gmail.com", password: "123456", phone_number: "0123456789", address: "Equality Street 38")
+
+puts "Admin-Username | #{x.username}"
+puts "Admin-Email    | #{x.email}"
+puts "Admin-password | #{x.password}"
+
+puts "..."
+
+puts "Creating Tools for admin"
+
+5.times do
+  Tool.create(name: tools.sample, description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true), user_id: x.id)
+end
+
+puts "Admin tools added"
+puts ".."
+puts "Creating more users and tools.."
+
+
+5.times do
+  puts "creating users"
+  u = User.create(username: Faker::Name.first_name, email: Faker::Internet.email, password: "123456", phone_number: Faker::PhoneNumber.cell_phone, address: Faker::Address.full_address)
+  puts "creating tools"
+  3.times do
+    Tool.create(name: tools.sample, description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true), user_id: u.id)
+  end
+end
 
 puts "Done"
