84f98986300e1f14ace079b4df5b3e4199bba89b
Danny Friese <danny.friese@posteo.de>
2020-02-26 16:30:10 +0100

    started map implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+  def index
+     @users = User.geocoded #returns flats with coordinates
+
+    @markers = @users.map do |flat|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
