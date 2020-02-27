9350fd10e23919478e9b571e18a60dbd53e7fd4c
Danny Friese <danny.friese@posteo.de>
2020-02-27 15:29:17 +0100

    bug fix try

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,8 @@ import "bootstrap";
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
 import 'flatpickr/dist/themes/airbnb.css'
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import { initMapbox } from '../plugins/init_mapbox';
+// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+// import { initMapbox } from '../plugins/init_mapbox';
 
 
 flatpickr('.datepickr', {
@@ -11,4 +11,4 @@ flatpickr('.datepickr', {
   disableMobile: "true"
 });
 
-initMapbox();
+// initMapbox();
